bc0d0d405944d83acbcbdcee472eacdbc66d9219
JaCkSoN <JaCkSoN@10.0.0.160>
2018-10-07 15:29:38 -0400

    Initial broken Simulation, still need to fix issues with job changing.

---
+++ Mezo Test/src/test/HolonList.java
@@ -0,0 +1,20 @@
+package test;
+
+import java.util.ArrayList;
+
+public interface HolonList {
+	//Holons
+	Holon Art = new Holon("Art");
+	Holon Construction = new Holon("Construction");
+	Holon Energy = new Holon("Energy");
+	Holon Judicial = new Holon("Judicial");
+	Holon Military = new Holon("Military");
+	Holon Education = new Holon("Education");
+	Holon Reserve = new Holon("Reserve");
+	Holon Crypto = new Holon("Crypto"); //This may not exist?
+	Holon Food = new Holon("Food");
+	Holon Transportation = new Holon("Transportation");
+	Holon Materials = new Holon("Materials");
+	Holon Health = new Holon("Health");
+	Holon Industrials = new Holon("Industrials");
+}
